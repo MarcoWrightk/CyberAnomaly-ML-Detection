5beea3f9f2c4da702264783cb36c58b5c2ef3743
Marco Velazquez <101480497+MarcoWrightk@users.noreply.github.com>
2025-09-25 14:15:36 -0600

    Update jenkins_pipeline.groovy

--- deployment /jenkins_pipeline.groovy
+++ deployment /jenkins_pipeline.groovy
@@ -1,1 +1,20 @@
+pipeline {
+  stages {
+    stage('Test') {
+      steps {
+        sh 'pytest tests/'
+      }
+    }
+    stage('Build') {
+      steps {
+        sh 'docker build -t anomaly-detector .'
+      }
+    }
+    stage('Deploy') {
+      steps {
+        sh './deploy.sh'
+      }
+    }
+  }
+}
 
